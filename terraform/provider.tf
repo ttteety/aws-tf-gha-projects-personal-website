6cf017d41c7584a0d21cf8fdce55829c3cd31a4b
ttteety <ttteety@yahoo.com>
2025-10-07 20:10:37 +1100

    remove dynamodb

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,6 @@ terraform {
         region = "ap-southeast-2"
         key = "github-actions/terraform.tfstate"
         encrypt = true
-        dynamodb_table = "tf-resources-gha-lock"
+        # dynamodb_table = "tf-resources-gha-lock"
     }
 }
