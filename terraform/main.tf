4f72b518e18ec1919341ab729f5d9ceeeb9a1ce3
ttteety <ttteety@yahoo.com>
2025-10-07 20:31:25 +1100

    use depend on

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,14 @@ resource "aws_s3_bucket_website_configuration" "static_website_config" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "static_site_access" {
+  bucket                  = aws_s3_bucket.static_site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "static_site_policy" {
   bucket = aws_s3_bucket.static_site.id
   policy = jsonencode({
@@ -23,12 +31,7 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
       }
     ]
   })
+
+  depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
 }
 
-resource "aws_s3_bucket_public_access_block" "static_site_access" {
-  bucket                  = aws_s3_bucket.static_site.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
